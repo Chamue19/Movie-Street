8b0a0e0fec1786d37d18051241f215a340581568
Chamue19 <115542082+Chamue19@users.noreply.github.com>
2024-05-29 17:20:45 +0530

    datafetching update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+
+import { useEffect } from "react";
+// use efect gttee meka run wen hmwelm apit api ek call krnn one nisa..
+// fe3f814c
+const url ='http://www.omdbapi.com/?apikey=fe3f814c';
 
 const App = () => {
+
+    const serchMovies = async (title) => {  // asyncronus function ekk ekiynne hoynn wel ynwa
+        const res = await fetch(`${url}&s=${title}`);
+        const data = await res.json();
+        console.log(data);
+    }
+
+    useEffect(() => {
+        serchMovies('avengers');
+    }, []);
     return(
         <div>
             <h1>APP</h1>
