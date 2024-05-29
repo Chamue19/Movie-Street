85ddcf72acfed55c51184392ad000ef3f8ee9d79
Chamue19 <115542082+Chamue19@users.noreply.github.com>
2024-05-29 17:03:06 +0530

    commit 1

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import react from 'react';
+import reactDOM from 'react-dom';
+
+import App from './App';
+
+//DOM ek initialize krnn one index.js eke
+reactDOM.render(<App />, document.getElementById('root'));
